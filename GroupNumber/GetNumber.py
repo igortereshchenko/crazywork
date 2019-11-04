efb3202ea05e359e90bf310980fc53a17a98d422
Your Name <you@example.com>
2019-11-04 15:56:31 +0200

    Realized function

--- GroupNumber/GetNumber.py
+++ GroupNumber/GetNumber.py
@@ -1,9 +1,18 @@
-def getNumber(row = 'No data'):
+import re
+def getNumber(text):
     """
     Функція зчитує рядок та повертає номер групи
 
     :param test: str, str, KM-91
     :return: str, str, 91
     """
-    print(row)
-    return None
+
+    group_number = re.findall('\d{2}', text)
+
+    return group_number
+
+from ParseRecordBook.Parser import parseRecordBook, getemaildata
+
+
+
+print( getNumber( getemaildata() ))
