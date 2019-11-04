0b57a20ed52309d92a3d52896b6706c3c3640406
igortereshchenko <igortereshchenko@gmail.com>
2019-11-04 14:47:16 +0200

    upload

---
+++ ParseRecordBook/Parser.py
@@ -0,0 +1,11 @@
+
+def parseRecordBook(test = 'No data'):
+    """
+    Для чого функція ???
+
+    :param test: data type, format, examples
+    :return: data type, format, examples
+    """
+
+    print(test)
+    return None
