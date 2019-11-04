1c44e297d3d12769a74924b0c4e8af821e611d22
EugeneBudzinskiy <eugene.unusual@gmail.com>
2019-11-04 15:24:38 +0200

    add GetMonth function

---
+++ GetMonth/__init__.py
@@ -0,0 +1,18 @@
+def get_month(text_string=None):
+
+    """
+    Ця функція з даного рядка, за шаблоном, виділяє місяць, та виводить його
+    :param text_string: string, 'число місяць рікр.', '1 вересня 2019р.' => 'вересень'
+    :return: string, 'місяць', 'вересень'
+    """
+
+    if text_string is None:
+
+        # Data is empty
+
+        return None
+    else:
+
+        # Do some stuff
+
+        return None
