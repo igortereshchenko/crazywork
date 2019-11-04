9596a22275985b7487d390bbc86d398f8a888b35
KateandEva <pavlovskakate2002@gmail.com>
2019-11-04 15:31:19 +0200

    First upload

---
+++ ParseFaculty/Faculty.py
@@ -0,0 +1,16 @@
+def parse_faculty(text):
+    """
+        Функція зчитування рядка факультету та виведення у короткому вигляді
+
+        :param text: data type: string
+               format: Word word word. Word
+               examples: Факультет прикладної математики. Денна
+                         Факультет інформаціонної обчислювальної техніки. Заочна
+      :return: data type: string
+               format: XXX
+               examples: ФПМ
+                         ФІОТ
+        """
+
+    print(text)
+    return None
